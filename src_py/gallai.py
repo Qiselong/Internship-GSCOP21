22e53dbdeddaa5c226bc7a198ade3f1c536a2911
Thomas B <boudier.tho@gmail.com>
2021-06-08 10:13:04 +0200

    Gallai 2

--- src_py/gallai.py
+++ src_py/gallai.py
@@ -11,16 +11,19 @@ import random
 
 #adjustment variable:
 space = 0.2     #space between intervals
-n = 40       #number of intervals
+n = 80       #number of intervals
 
-def plot_intervals(L):
+def plot_intervals(L, title = ''):
     red = 0 #how much red intervals?
     for i in range(len(L)):
         interval = L[i]
         plt.plot([interval[0], interval[1]], [space*i, space*i], interval[2])
         if interval[2] != 'k':
             red +=1
-    plt.title("Transversal set with " + str(red)+" points")
+    if title == '':
+        plt.title(r'$\nu$' + '=' + str(red) + ' ; ' + str(len(L)) + " total sets")
+    else:
+        plt.title(title)
     plt.show()
     
 def generate_intervals(n):
@@ -39,7 +42,7 @@ def sort_intervals(L, method='right'):
     '''
     sort the intervals by the method end point
     '''
-    dtype1 = [('left', float), ('right', float), ('color', 'U5')] #see: https://numpy.org/doc/stable/reference/generated/numpy.sort.html
+    dtype1 = [('left', float), ('right', float), ('color', 'U20')] #see: https://numpy.org/doc/stable/reference/generated/numpy.sort.html
     LNP = np.array(L, dtype = dtype1)
     L = np.sort(LNP, order = method)
     return L
@@ -50,10 +53,11 @@ def isin(a,b,x):
     '''
     return a< x and x<b
 
-def coloration(L, color = 'r'):
+def max_set(L, color = 'r'):
     '''
-    coloration using the algorithm described in Gallai's proof.
-    The disjoins intervals appear in 
+    find a maximal disjoint set using the algorithm described in Gallai's proof.
+    It is equal to the hitting number.
+    The disjoins intervals appear in red
     '''
     X=[]
     for i in range(len(L)):
@@ -66,5 +70,39 @@ def coloration(L, color = 'r'):
             L[i][2] = color
     return L
 
+colors16 = ['r', 'g', 'b', 'c', 'y', 'm', 'tab:blue', 'tab:orange', 'tab:green', 'tab:red','tab:purple', 'tab:brown','tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan', '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
+
+def coloration(L, colors=colors16):
+    '''
+    L: list of intervals.
+    colors: list of color names.
+    Does the colloration following the second proof of Gallai.
+    '''
+    L = sort_intervals(L, 'left')
+    col_used = 0 #number of color used
+    for i in range(len(L)):
+        #then we try to eliminate every color by checking if L[i] is intersecting with an other interval of a given color. it
+        flagcolored = False # 'have we figured out a coloration  for L[i] with already an used color?'
+        for col in range(col_used):
+            flag = False # 'has this color been used in an intersecting interval?'
+            for j in range(i):
+                if L[j][2] == colors[col] and isin(L[j][0], L[j][1], L[i][0]):
+                    flag = True
+                    break
+            if flag == False:
+                #then we can use color to color L[i]
+                L[i][2] = colors[col]
+                flagcolored=True
+                break
+            # if flag == True we check with an other interval
+        if flagcolored == False:
+            col_used +=1
+            L[i][2] = colors[col_used]
+    return L
+        
+
+            
 ## Execution
-plot_intervals(coloration(sort_intervals(generate_intervals(n))))
+plot_intervals(max_set(sort_intervals(generate_intervals(n))))
+
+plot_intervals(coloration(generate_intervals(15)), title='coloration using Gallai\'s algorithm')
